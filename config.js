bea96081ce4d05a6454c2a327de1289836c58873
BarefootVagabond <47724172+BarefootVagabond@users.noreply.github.com>
2025-08-12 20:53:31 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Set this to your deployed backend base URL (from Vercel), ending in /api
 // Example: 'https://khayrah-ai-backend.vercel.app/api'
-window.API_BASE = 'https://khayrah-ai-backend-rf8s.vercel.app/api';
+window.API_BASE = 'https://khayrah-ai-backend-rf8s-f4jhrox8m-barefoots-projects-8f7e964e.vercel.app/api';
 // leave empty to use offline dataset only
